ed197e0c80a756bd9b29831768c3039d559136fe
Alyssa Senatin <alyssasenatin@gmail.com>
2025-01-14 01:44:03 +0800

    Remove import index.css from main

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './styles/bootstrap.scss';
-import './index.css';
+
 import App from './App.tsx';
+
+import './styles/bootstrap.scss';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
