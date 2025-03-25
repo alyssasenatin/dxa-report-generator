5037c220a4361ee191707f76f45a6ddc3382788e
Alyssa Senatin <alyssasenatin@gmail.com>
2025-03-25 20:15:11 +0800

    Remove unnecessary console log in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -10,8 +10,3 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </StrictMode>
 );
-
-// Use contextBridge
-window.ipcRenderer.on('main-process-message', (_event, message) => {
-  console.log(message);
-});
